812968ea89444e997aeacb6f2bdad03fc6b533f8
semialcruz <semialcruz@gmail.com>
2021-06-08 02:28:43 -0600

    Prod

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
     include_once('classes/APPlogin.php');
     $APPlogin = new APPlogin();
     //include_once('views/header/header.php');
