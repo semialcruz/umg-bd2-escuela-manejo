b853645b6d28e2afed4e85653e817065ccd06921
semialcruz <semialcruz@gmail.com>
2021-06-08 02:23:32 -0600

    Agregar scripts y estilos adicionales

--- views/footer/footer.php
+++ views/footer/footer.php
@@ -6,7 +6,9 @@
     <script src="<?php echo APPLOGIN_LOCATION; ?>views/js/jquery-3.6.0.min.js" type="text/javascript"></script>
     <script src="<?php echo APPLOGIN_LOCATION; ?>views/js/bootstrap.bundle.min.js" type="text/javascript"></script>
     <script src="<?php echo APPLOGIN_LOCATION; ?>views/js/fullcalendar.min.js" type="text/javascript"></script>
+    <script src="<?php echo APPLOGIN_LOCATION; ?>views/js/locales/fullcalendar.es.js" type="text/javascript"></script>
     <script src="<?php echo APPLOGIN_LOCATION; ?>views/js/bootstrap-datepicker.js" type="text/javascript"></script>
+    <script src="<?php echo APPLOGIN_LOCATION; ?>views/js/jquery.dataTables.min.js" type="text/javascript"></script>
     <script src="<?php echo APPLOGIN_LOCATION; ?>views/js/main.js" type="text/javascript"></script>
 
     </body>
--- views/js/main.js
+++ views/js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
     $('#catBtn').click(function (){
         $('.home').hide();
+        //$('.reports').hide();
+    });
+
+    $('#reBtn').click(function (){
+        $('.home').hide();
+        //$('.catalogs').hide();
+
     });
 
     $('.fecha').datepicker({
@@ -17,7 +24,7 @@ $(document).ready(function () {
             data: $(this).serialize(),
             success: function (response){
                 var jsonData = JSON.parse(response);
-
+                console.log(jsonData);
                 if (jsonData.exito){
                     $('#alerta').addClass('alert-success');
                     $('#alerta').html(
@@ -27,15 +34,80 @@ $(document).ready(function () {
                     );
                     $('#new-client').trigger("reset");
                 }else{
-                    $('#alerta').addClass('alert-success');
+                    $('#alerta').addClass('alert-warning');
                     $('#alerta').html(
-                        '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>'+
-                        'Los datos del cliente se ingresaron con éxito'+
+                        '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>'+
+                        'Algo salió mal y los datos no pudieron guardarse. Por favor intente de nuevo'+
                         '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
                     );
                 }
             }
         });
     });
+
+    $('#new-personal').submit(function (e){
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url : 'crud/save.php',
+            data: $(this).serialize(),
+            success: function (response){
+                var jsonData = JSON.parse(response);
+                console.log(jsonData);
+                if (jsonData.exito){
+                    $('#alerta').addClass('alert-success');
+                    $('#alerta').html(
+                        '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>'+
+                        'Los datos del nuevo personal se ingresaron con éxito'+
+                        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+                    );
+                    $('#new-client').trigger("reset");
+                }else{
+                    $('#alerta').addClass('alert-warning');
+                    $('#alerta').html(
+                        '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>'+
+                        'Algo salió mal y los datos no pudieron guardarse. Por favor intente de nuevo'+
+                        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+                    );
+                }
+            }
+        });
+    });
+
+    $('#eventGrid').DataTable( {
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "../../crud/select.php?table=1",
+            "type": "POST"
+        },
+        "columns": [
+            { "data": "no" },
+            { "data": "cliente" },
+            { "data": "telefono" },
+            { "data": "email" },
+            { "data": "fecha_inicio" },
+            { "data": "fecha_fin" },
+            { "data": "instructor" },
+            { "data": "sesiones" }
+        ]
+    } );
+
+    $('#contractGrid').DataTable( {
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "../../crud/select.php?contrato=1",
+            "type": "POST"
+        },
+        "columns": [
+            { "data": "numero_contrato" },
+            { "data": "numero_sesiones" },
+            { "data": "instructor" },
+            { "data": "vehiculo" },
+            { "data": "fecha_inicio" },
+            { "data": "fecha_fin" }
+        ]
+    } );
 
 });
