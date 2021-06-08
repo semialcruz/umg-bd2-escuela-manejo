060d9128ad7951797bb0b9e5830f23cc3a46e10e
semialcruz <semialcruz@gmail.com>
2021-06-08 02:28:08 -0600

    Server prod

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-//define("SERVER", "umgserver.southcentralus.cloudapp.azure.com");
-define("SERVER", "localhost");
+define("SERVER", "umgserver.southcentralus.cloudapp.azure.com");
+//define("SERVER", "localhost");
 define("DATABASE","Escuela_Manejo");
 define("DBUSER","umg");
 define("DBPWD","123");
